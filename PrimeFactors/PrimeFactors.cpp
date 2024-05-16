5140343e51bc7b7eddf42dc1f5e1eb7e84741c56
quokkaHunter <ekdlemdjq456@naver.com>
2024-05-16 16:47:17 +0900

    [Feature] Page 37

---
+++ PrimeFactors/PrimeFactors.cpp
@@ -0,0 +1,15 @@
+﻿#include <vector>
+
+using namespace std;
+
+class PrimeFactor
+{
+public:
+	vector<int> op(int n)
+	{
+		vector<int> result = {};
+		if (n == 2)
+			result.push_back(2);
+		return result;
+	}
+};
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,14 @@
 ﻿#include "pch.h"
+#include "../PrimeFactors/PrimeFactors.cpp"
+#include <vector>
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(PrimeFactor, Op1) {
+	PrimeFactor primeFactor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, primeFactor.op(1));
+}
+TEST(PrimeFactor, Op2) {
+	PrimeFactor primeFactor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, primeFactor.op(2));
 }
