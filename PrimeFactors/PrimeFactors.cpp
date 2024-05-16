1d540565cc39d7260908c9a3bc795468e36911ac
quokkaHunter <ekdlemdjq456@naver.com>
2024-05-16 16:58:08 +0900

    [Refactoring] Page 46

--- PrimeFactors/PrimeFactors.cpp
+++ PrimeFactors/PrimeFactors.cpp
@@ -12,8 +12,12 @@ public:
 		{
 			if (n == 4)
 			{
-				result.push_back(2);
-				result.push_back(2);
+				do
+				{
+					if (n % 2 == 0)
+						result.push_back(2);
+					n = n / 2;
+				} while (n > 0);
 			}
 			else
 				result.push_back(n);
