3b91510d4e783f7df4289292dc6058d3185608f6
quokkaHunter <ekdlemdjq456@naver.com>
2024-05-17 08:40:53 +0900

    [Refactoring] Page 59

--- PrimeFactors/PrimeFactors.cpp
+++ PrimeFactors/PrimeFactors.cpp
@@ -12,25 +12,13 @@ public:
 		if (n < 2)
 			return result;
 
-		while (n % 2 == 0)
+		for (int i = 2; n > 1; i++)
 		{
-			result.push_back(2);
-			n /= 2;
-		}
-		while (n % 3 == 0)
-		{
-			result.push_back(3);
-			n /= 3;
-		}
-		while (n % 5 == 0)
-		{
-			result.push_back(5);
-			n /= 5;
-		}
-		while (n % 7 == 0)
-		{
-			result.push_back(7);
-			n /= 7;
+			while (n % i == 0)
+			{
+				result.push_back(i);
+				n /= i;
+			}
 		}
 		return result;
 	}
